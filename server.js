1a1d6c4a369e63b453bea6697d55d193077e243a
duckheewon <fain9301@yahoo.com>
2017-09-08 11:35:40 +0900

    testing

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+ *   사진 및 센서 정보를 하여 서버로 전송
+ *
+ *   1. 카메라기능
+ *   2. 센싱 기능
+ *   3. MQTT 사용자 명령 확인
+ *
+ */
+
+var camera = require("./camera/photo"); //사진 촬영 모듈
+//var arduino = require("./camera/arduino"); //arduino
+
+var controller = require('./camera/camera');
+
+controller.start();
+
+//camera.start(); //사진 촬영 시작
